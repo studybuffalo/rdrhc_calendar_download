c19321f98c819131578458f2ccef0b2886adc048
studybuffalo <studybuffalo@gmail.com>
2022-06-19 09:05:20 -0600

    Adding debug code

--- download/session.py
+++ download/session.py
@@ -22,9 +22,11 @@ def setup_session(config, log):
     # Setup Selenium webdriver
     with webdriver.Firefox(service=Service(config.download.driver_path)) as driver:
         # Make initial call and get the login page
+        log.debug('Calling login page via Selenium webdriver.')
         driver.get(config.download.schedule_home)
 
         # Retrieve each element (wait 5 seconds for timeout)
+        log.debug('Retrieving references to login form via Selenium webdriver.')
         user_name_field = WebDriverWait(driver, 5).until(
             expected_conditions.presence_of_element_located((By.ID, 'login'))
         )
@@ -36,16 +38,19 @@ def setup_session(config, log):
         )
 
         # Fill out fields and submit form
+        log.debug('Filling out login form via Selenium webdriver.')
         user_name_field.send_keys(config.download.schedule_user)
         password_field.send_keys(config.download.schedule_password)
         submit_button.click()
 
         # Wait for the home page to load to allow cookies to be set
+        log.debug('Waiting for home page to load.')
         WebDriverWait(driver, 5).until(
             expected_conditions.url_to_be(config.download.schedule_home)
         )
 
-        # Copy cookies from selenium to a request session
+        # Copy cookies from Selenium to a requests session
+        log.debug('Copying cookies from Selenium to requests session.')
         session = requests.Session()
         selenium_cookies = driver.get_cookies()
 
